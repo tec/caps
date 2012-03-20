80f592ef6b4261fef1dca50caa0cf42f022db617
Silvan Golega <silvan@golega.de>
2012-03-20 21:38:05 +0100

    get availaility/workload model methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ActiveRecord::Base
+  has_many :workloads
+  has_many :assignments
+  
+  def get_workload year, week
+    Workload.
+      where( :project_id => self.id ).
+      where( "from_year <= ?", year ).
+      where( "from_week <= ?", week ).
+      order( :from_year, :from_week ).
+      last
+  end
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,14 @@
 class Worker < ActiveRecord::Base
+
+  has_many :availabilities
+  has_many :assignments
+
+  def get_availability year, week
+    Availability.
+      where( :worker_id => self.id ).
+      where( "from_year <= ?", year ).
+      where( "from_week <= ?", week ).
+      order( :from_year, :from_week ).
+      last
+  end
 end
